4425f0f93b93896c436ff464bb7f1e926f874991
adriano.pogianeli@gmail.com <adriano.pogianeli@gmail.com>
2019-01-04 02:12:45 +0000

    some modules

---
+++ code/environments/production/manifests/app.pp
@@ -0,0 +1,23 @@
+node 'node1' {
+
+    notify{"**** NODE1 BLOCK ****":}
+
+    include java::install
+
+}
+
+node 'node2' {
+
+    notify{"**** NODE2 BLOCK ****":}
+
+    include java::install
+
+}
+
+node default {
+
+    notify{"checkpoint_1":
+        message => " **** DEFAULT BLOCK **** " 
+    }
+
+}
