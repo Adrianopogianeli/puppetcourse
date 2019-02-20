0cb27e13a428efbfab97babad7a572ffb25fb2c3
adriano.pogianeli@gmail.com <adriano.pogianeli@gmail.com>
2019-02-20 01:37:32 +0000

    add file tomcat.conf

--- code/environments/production/modules/tomcat/manifests/init.pp
+++ code/environments/production/modules/tomcat/manifests/init.pp
@@ -1,8 +1,11 @@
 
 class tomcat {
 
+    notify{"checkpoint_tomcat_init":}
+
     include java::install
     include tomcat::install
+    include tomcat::config
     include tomcat::service
 
     Class['java::install'] -> Class['tomcat::install'] ~> Class['tomcat::service']
